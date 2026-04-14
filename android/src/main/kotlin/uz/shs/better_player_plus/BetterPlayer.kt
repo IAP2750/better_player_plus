34f0c2e7277aff0889223203ddb3b15e6e903e89
Serhii Horbenko <shorben@softserveinc.com>
2026-04-14 09:49:00 +0300

    layerNotificationManager.setMediaSessionToken() in media3 1.4+ changed its signature to accept the platform android.media.session.MediaSession.Token instead of MediaSessionCompat.Token. The fix calls .token on the MediaSessionCompat.Token to get the underlying platform token and casts it to the expected type. Since minSdkVersion is 21, the platform MediaSession.Token is always available

--- android/src/main/kotlin/uz/shs/better_player_plus/BetterPlayer.kt
+++ android/src/main/kotlin/uz/shs/better_player_plus/BetterPlayer.kt
@@ -328,7 +328,7 @@ internal class BetterPlayer(
             }
 
             setupMediaSession(context)?.let {
-                setMediaSessionToken(it.sessionToken)
+                setMediaSessionToken(it.sessionToken.token as android.media.session.MediaSession.Token)
             }
         }
 
